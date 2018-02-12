2274f62864e43b4810142dc7b84fb84a4646c2f8
Kaila Goff and Masen Matthews <kaila@>
2018-02-12 10:37:21 -0800

    fix error message, app working properly

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('pry')
+require('./lib/triangle')
 
 get('/') do
   "This application will test whether-or-not inputted values make up a triangle"
@@ -11,5 +12,8 @@ get('/output') do
   @side1 = params.fetch("side1")
   @side2 = params.fetch("side2")
   @side3 = params.fetch("side3")
+  output = Tri.new(@side1, @side2, @side3)
+  # binding.pry
+  @three_sides = output.triangles(@side1, @side2, @side3)
   erb(:output)
 end
